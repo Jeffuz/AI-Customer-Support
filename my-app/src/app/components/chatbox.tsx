e1ce763c457042a446b21ccd92dbffe7d1962956
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-08-09 11:19:59 +0800

    update styling for main

--- my-app/src/app/components/chatbox.tsx
+++ my-app/src/app/components/chatbox.tsx
@@ -9,20 +9,18 @@ interface Message {
   sender: "user" | "other";
 }
 const GetResponse = async (messageToSend: String) => {
-  const data = {message: messageToSend}
+  const data = { message: messageToSend };
   const headers = {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
-
-  }
+    body: JSON.stringify(data),
+  };
   return await fetch("/api/genMsg", headers)
-  .then(response => response.json())
-  .catch(e => console.log(e)); 
-
-}
+    .then((response) => response.json())
+    .catch((e) => console.log(e));
+};
 
 const Chatbox = () => {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -37,18 +35,25 @@ const Chatbox = () => {
       setMessages([...messages, userMessage]);
       setInput("");
 
-      // take response.content 
-      const response = await GetResponse(input)
+      // take response.content
+      const response = await GetResponse(input);
 
-      if(response.success) {
-        const aiResponse = response.body.message.content
-        console.log(aiResponse)
-        setMessages((prevMessages) => [...prevMessages, {text: aiResponse, sender: "other"}]);
+      if (response.success) {
+        const aiResponse = response.body.message.content;
+        console.log(aiResponse);
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { text: aiResponse, sender: "other" },
+        ]);
       } else {
-        setMessages((prevMessages) => [...prevMessages, {text: "There was an error with generating our statement", sender: "other"}])
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          {
+            text: "There was an error with generating our statement",
+            sender: "other",
+          },
+        ]);
       }
-
-
     }
   };
 
@@ -65,9 +70,13 @@ const Chatbox = () => {
       {/* Message box */}
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((msg, index) => (
+          // Messages
           <div
             key={index}
+            // w-max: set max width based on child
+            // max-w-75%: max possible width of message
             className={`flex w-max max-w-[75%] rounded-lg px-3 py-2 text-sm ${
+              // user messages are blue, other is gray
               msg.sender === "user"
                 ? "ml-auto bg-blue-500 text-white"
                 : "bg-gray-200"
@@ -81,12 +90,16 @@ const Chatbox = () => {
       {/* Submission */}
       <div className="border-t px-4 py-3">
         <form onSubmit={handleSend} className="flex items-center w-full gap-2">
+          {/* Input for textfield */}
           <input
             placeholder="Type your message..."
             value={input}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setInput(e.target.value);
+            }}
             className="flex px-4 py-2 border rounded w-full focus:outline-none"
           />
+          {/* Submit button */}
           <button
             type="submit"
             className="p-2 bg-blue-600 hover:bg-blue-700 text-white rounded-full"
