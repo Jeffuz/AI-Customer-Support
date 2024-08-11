9f7dd05d4798b66455c4a53d870afca5ece305c7
Mathew Raju <mathew2002raj@gmail.com>
2024-08-10 19:30:19 -0700

    Yes

--- my-app/src/app/signup/page.tsx
+++ my-app/src/app/signup/page.tsx
@@ -4,28 +4,12 @@
 
 import { FaMountain } from "react-icons/fa"
 import { useRouter } from 'next/navigation';
-import { useState } from "react";
-import { signUp } from "../firebase/auth";
 
 // portion to create user data and send over to firebase. This data will then be obtained from sign in
 
-const SignUp = () => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
+const Signup = () => {
 
   const router = useRouter(); // Initialize the useRouter hoo
-  const handleSignUp = async(e: React.FormEvent) =>{
-    e.preventDefault();
-    try {
-      await signUp(email, password);
-      alert('Signed up successfully');
-    } catch (error) {
-      setError('Failed to sign up');
-    }
-  }
   const handleClick = () => {
     router.push('/signin')
   }
@@ -48,8 +32,6 @@ const SignUp = () => {
               <label className="block text-gray-700">Email</label>
               <input
                 type="email"
-                value = {email}
-                onChange = {(e) => setEmail(e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 rounded-lg"
                 placeholder="Enter your email"
               />
@@ -58,15 +40,12 @@ const SignUp = () => {
               <label className="block text-gray-700">Password</label>
               <input
                 type="password"
-                value = {password}
-                onChange = {(e) => setPassword(e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 rounded-lg"
                 placeholder="Enter your password"
               />
             </div>
             <button
               type="submit"
-              onClick={handleSignUp}
               className="w-full bg-blue-600 text-white px-4 py-2 rounded-lg"
             >
               Sign Up
@@ -85,4 +64,4 @@ const SignUp = () => {
   );
 }
 
-export default SignUp
+export default Signup
